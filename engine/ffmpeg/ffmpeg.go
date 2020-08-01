46da0e376fc9d7a3689b88105dd55cbf3f5a66f6
cgnq； <cgnq@163.com>
2020-08-01 15:25:31 +0800

    5

--- engine/ffmpeg/ffmpeg.go
+++ engine/ffmpeg/ffmpeg.go
@@ -14,18 +14,19 @@ type Ff struct {
 }
 
 func Tomp4(inputPath, outputPath string) {
+	outputPath = outputPath + ".mp4"
 	log.Println("input-output:", inputPath, outputPath)
 	// Create new instance of transcoder
 	trans := new(transcoder.Transcoder)
-	//trans.MediaFile().SetVideoCodec("xvid")
 	//f := ffmpeg.Configuration{FfprobeBin: "E:/worktool/ffmpeg/bin/ffprobe.exe ", FfmpegBin: "E:/worktool/ffmpeg/bin/ffmpeg.exe "}
 	//trans.SetConfiguration(f)
 	err := trans.Initialize(inputPath, outputPath)
+	log.Println("err:", err)
+	trans.MediaFile().SetVideoCodec("xvid")
 	trans.MediaFile().SetResolution("480x320")
 	trans.MediaFile().SetVideoBitRate("400k")
 	trans.MediaFile().SetFrameRate(25)
 
-	log.Println("err:", err)
 	// Handle error...
 
 	// Start transcoder process without checking progress
